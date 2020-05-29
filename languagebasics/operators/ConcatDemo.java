24f874ce0b8654d3639f87d66f60edc1587195be
Saranyan Senthivel <saran.s6691@gmail.com>
2020-05-29 12:31:06 -0500

    completed Assignment-Arithmetic-Unary Operators

---
+++ languagebasics/operators/ConcatDemo.java
@@ -0,0 +1,11 @@
+package languagebasics.operators;
+
+class ConcatDemo {
+    public static void main(String[] args) {
+        String firstString = "This is";
+        String secondString = " a concatenated String.";
+        String thirdString = firstString + secondString;
+        System.out.println(thirdString);
+    }
+    
+}
